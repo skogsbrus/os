ab141450c93c2c4441563988a0b0ace34a6b724d
Johan Andersson <johan-andersson01@protonmail.com>
2021-07-20 20:05:21 +0200

    Add power options

--- devices/lenovo-p1.nix
+++ devices/lenovo-p1.nix
@@ -54,4 +54,39 @@
   # Maybe prevents crash?
   # Jul 20 11:17:33 voidm kernel: nouveau 0000:01:00.0: fifo: fault 01 [VIRT_WRITE] at 0000000008000000 engine 40 [gr] client 13 [GPC0/PROP_0] reason 00 [PDE] on channel 6 [00feff3000 Xwayland[1859]]
   services.xserver.videoDrivers = [ "modesetting" ];
+
+  # power saving options
+  boot.kernelParams = ["intel_pstate=disable"];
+  services.tlp = {
+    enable = true;
+    extraConfig = ''
+      START_CHARGE_THRESH_BAT0=75
+      STOP_CHARGE_THRESH_BAT0=80
+
+      CPU_SCALING_GOVERNOR_ON_AC=schedutil
+      CPU_SCALING_GOVERNOR_ON_BAT=schedutil
+
+      CPU_SCALING_MIN_FREQ_ON_AC=800000
+      CPU_SCALING_MAX_FREQ_ON_AC=3500000
+      CPU_SCALING_MIN_FREQ_ON_BAT=800000
+      CPU_SCALING_MAX_FREQ_ON_BAT=2300000
+
+      # Enable audio power saving for Intel HDA, AC97 devices (timeout in secs).
+      # A value of 0 disables, >=1 enables power saving (recommended: 1).
+      # Default: 0 (AC), 1 (BAT)
+      SOUND_POWER_SAVE_ON_AC=0
+      SOUND_POWER_SAVE_ON_BAT=1
+
+      # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
+      # Default: on (AC), auto (BAT)
+      RUNTIME_PM_ON_AC=on
+      RUNTIME_PM_ON_BAT=auto
+
+      # Battery feature drivers: 0=disable, 1=enable
+      # Default: 1 (all)
+      NATACPI_ENABLE=1
+      TPACPI_ENABLE=1
+      TPSMAPI_ENABLE=1
+    '';
+  };
 }
