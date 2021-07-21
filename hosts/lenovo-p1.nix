0e0448a1de011d6bda49a6b2342b5e9ce17bd35c
Johan Andersson <johan-andersson01@protonmail.com>
2021-07-22 00:05:55 +0200

    Separate tlp from lenovo config

--- hosts/lenovo-p1.nix
+++ hosts/lenovo-p1.nix
@@ -4,7 +4,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+      ../sys/tlp.nix
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
@@ -75,34 +77,4 @@
 
   # power saving options
   services.power-profiles-daemon.enable = false;
-  services.tlp = {
-    enable = true;
-    settings.START_CHARGE_THRESH_BAT0=75;
-    settings.STOP_CHARGE_THRESH_BAT0=80;
-
-    settings.CPU_SCALING_GOVERNOR_ON_AC="schedutil";
-    settings.CPU_SCALING_GOVERNOR_ON_BAT="schedutil";
-
-    settings.CPU_SCALING_MIN_FREQ_ON_AC=800000;
-    settings.CPU_SCALING_MAX_FREQ_ON_AC=3500000;
-    settings.CPU_SCALING_MIN_FREQ_ON_BAT=800000;
-    settings.CPU_SCALING_MAX_FREQ_ON_BAT=2300000;
-
-    # Enable audio power saving for Intel HDA, AC97 devices (timeout in secs).
-    # A value of 0 disables, >=1 enables power saving (recommended: 1).
-    # Default: 0 (AC), 1 (BAT)
-    settings.SOUND_POWER_SAVE_ON_AC=0;
-    settings.SOUND_POWER_SAVE_ON_BAT=1;
-
-    # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
-    # Default: on (AC), auto (BAT)
-    settings.RUNTIME_PM_ON_AC="on";
-    settings.RUNTIME_PM_ON_BAT="auto";
-
-    # Battery feature drivers: 0=disable, 1=enable
-    # Default: 1 (all)
-    settings.NATACPI_ENABLE=1;
-    settings.TPACPI_ENABLE=1;
-    settings.TPSMAPI_ENABLE=1;
-  };
 }
