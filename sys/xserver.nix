80a708e8effb753ff0aacd8bede6b5e95c44c31f
Johan Andersson <johan-andersson01@protonmail.com>
2021-07-22 13:46:26 +0200

    Add gnome keybindings & settings

--- sys/xserver.nix
+++ sys/xserver.nix
@@ -8,9 +8,28 @@
     # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
 
-    # Enable the GNOME 3 Desktop Environment.
+    # Enable the GNOME Desktop Environment.
     displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+      # Keybindings / settings
+      # Dumped with `dconf dump /org/gnome/settings-daemon/plugins/`
+      extraGSettingsOverrides = ''
+        [color]
+        night-light-enabled=true
+        night-light-temperature=uint32 3700
+
+        [media-keys]
+        custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+
+        [media-keys/custom-keybindings/custom0]
+        binding='<Super>Return'
+        command='gnome-terminal'
+        name='Open terminal'
+      '';
+    };
 
     # Add custom keyboard layout
     extraLayouts.us-swedish = {
