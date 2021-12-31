76cd3b587d897976ea8362fd0dc577cabd5a4f15
Johan Andersson <johan-andersson01@protonmail.com>
2021-12-31 12:06:12 +0000

    Add workstation config

--- flake.nix
+++ flake.nix
@@ -7,20 +7,37 @@
 
   outputs = { self, nixpkgs, unstable, home-manager, ... }:
   {
-    nixosConfigurations.voidm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/lenovo-p1.nix
-        ./sys
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.johanan = { ... }: {
-            _module.args.unstable = unstable;
-            imports = [ ./home ];
-          };
-        }
-      ];
+    nixosConfigurations = {
+      voidm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/lenovo-p1.nix
+          ./sys
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.johanan = { ... }: {
+              _module.args.unstable = unstable;
+              imports = [ ./home ];
+            };
+          }
+        ];
+      };
+      workstation = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/workstation.nix
+          ./sys
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.johanan = { ... }: {
+              _module.args.unstable = unstable;
+              imports = [ ./home ];
+            };
+          }
+        ];
+      };
     };
   };
 }
---
+++ hosts/workstation.nix
@@ -0,0 +1,64 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    efiSupport = true;
+    enableCryptodisk = true;
+    device = "nodev";
+    useOSProber = true;
+  };
+
+  # Encryption setup
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/d9e29ce6-a8ca-423e-ae67-ea36df87ec4f";
+      preLVM = true;
+    };
+  };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/85e42b7e-cdb2-43d5-b8b9-45246ff96006";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/7CEF-102A";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/38f1afc0-e3d0-4389-91f3-227d44b75911"; }
+    ];
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp35s0.useDHCP = true;
+  networking.interfaces.wlp39s0.useDHCP = true;
+  networking.hostName = "workstation"; # Define your hostname.
+}
