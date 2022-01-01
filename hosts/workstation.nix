1ad7a1816d2d9c768a2f5380e8c4654533fdb0e7
Johan Andersson <johan-andersson01@protonmail.com>
2022-01-01 13:45:41 +0100

    workstation: add nvidia drivers

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -46,14 +46,6 @@
   # high-resolution display
   hardware.video.hidpi.enable = lib.mkDefault true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
@@ -61,4 +53,14 @@
   networking.interfaces.enp35s0.useDHCP = true;
   networking.interfaces.wlp39s0.useDHCP = true;
   networking.hostName = "workstation"; # Define your hostname.
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
 }
