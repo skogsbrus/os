2e55dc41cf0cc6e27bc1a28f253065931c187cd1
Johan Andersson <johan-andersson01@protonmail.com>
2022-01-08 17:36:08 +0100

    Postgres: update policy
    Previous policy didn't work with Ecto in Phoenix projects.

--- sys/postgres.nix
+++ sys/postgres.nix
@@ -4,10 +4,13 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_10;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      local all all trust
-      host all all ::1/128 trust
+    enableTCPIP = false;
+    authentication = pkgs.lib.mkForce ''
+      # Generated file; do not edit!
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            trust
+      host    all             all             ::1/128                 trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE johanan WITH LOGIN PASSWORD 'postgres' CREATEDB;
