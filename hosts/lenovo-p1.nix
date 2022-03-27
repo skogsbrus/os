311347dc184ed1d4aa4e339519ef6cf93a507d14
Johan Andersson <johan-andersson01@protonmail.com>
2022-03-27 19:57:51 +0200

    udev experiment

--- hosts/lenovo-p1.nix
+++ hosts/lenovo-p1.nix
@@ -10,8 +10,7 @@
     ];
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="4255", ATTR{idProduct}=="0012" MODE="0666"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="4255", ATTR{idProduct}=="0014" MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="4255", MODE="0666"
   '';
 
   virtualisation.libvirtd.enable = true;
