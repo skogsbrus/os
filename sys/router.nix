e06098aac66fdf1585d8e2a3c22cf3ae492d150d
Johan Andersson <johan-andersson01@protonmail.com>
2022-05-07 23:08:38 +0200

    Revert "Attempt"
    This reverts commit 19956b78dbd560ec60bc3911b58dce01a003e96b.

--- sys/router.nix
+++ sys/router.nix
@@ -67,87 +67,21 @@
     # a: ~10Mbit/s
     ssid = "beepboop";
     extraConfig = ''
-      ### hostapd configuration file
-      driver=nl80211
-
-      ### IEEE 802.11
-      ssid=beepboop
-      hw_mode=a
+      beacon_int=100
       channel=36
-      max_num_sta=128
-      auth_algs=1
-      disassoc_low_ack=1
-
-      ### DFS
-      ieee80211h=1
-      ieee80211d=1
       country_code=SE
-
-      ### IEEE 802.11n
-      ieee80211n=1
-      ht_capab=[HT40+][SHORT-GI-40][TX-STBC][RX-STBC1][DSSS_CCK-40]
-
-      ### IEEE 802.11ac
-      ieee80211ac=1
-      vht_oper_chwidth=1
-      vht_oper_centr_freq_seg0_idx=42
-      # compared against iw list, antenna patterns unclear
-      vht_capab=[TX-STBC-2BY1][RX-STBC-1][MAX-A-MPDU-LEN-EXP3][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]
-
-      ### WPA/IEEE 802.11i
-      wpa=2
+      driver=nl80211
+      hw_mode=a
+      #ieee80211ac=1
+      #ieee80211n=1
       wpa_key_mgmt=WPA-PSK
       wpa_passphrase=foobar123
       wpa_pairwise=CCMP
-
-      ### hostapd event logger configuration
-      logger_syslog=-1
-      logger_syslog_level=1
-      logger_stdout=-1
-      logger_stdout_level=1
-
-      ### WMM
+      logger_syslog=1 # debug
+      require_vht=1
+      #vht_capab=[MAX-MPDU-11454][SHORT-GI-80][TX-STBC-12][RX-STBC-1][SU-BEAMFORMEE][HTC-VHT]
+      vht_oper_centr_freq_seg0_idx=42
       wmm_enabled=1
-      uapsd_advertisement_enabled=1
-      wmm_ac_bk_cwmin=4
-      wmm_ac_bk_cwmax=10
-      wmm_ac_bk_aifs=7
-      wmm_ac_bk_txop_limit=0
-      wmm_ac_bk_acm=0
-      wmm_ac_be_aifs=3
-      wmm_ac_be_cwmin=4
-      wmm_ac_be_cwmax=10
-      wmm_ac_be_txop_limit=0
-      wmm_ac_be_acm=0
-      wmm_ac_vi_aifs=2
-      wmm_ac_vi_cwmin=3
-      wmm_ac_vi_cwmax=4
-      wmm_ac_vi_txop_limit=94
-      wmm_ac_vi_acm=0
-      wmm_ac_vo_aifs=2
-      wmm_ac_vo_cwmin=2
-      wmm_ac_vo_cwmax=3
-      wmm_ac_vo_txop_limit=47
-      wmm_ac_vo_acm=0
-
-      ### TX queue parameters
-      tx_queue_data3_aifs=7
-      tx_queue_data3_cwmin=15
-      tx_queue_data3_cwmax=1023
-      tx_queue_data3_burst=0
-      tx_queue_data2_aifs=3
-      tx_queue_data2_cwmin=15
-      tx_queue_data2_cwmax=63
-      tx_queue_data2_burst=0
-      tx_queue_data1_aifs=1
-      tx_queue_data1_cwmin=7
-      tx_queue_data1_cwmax=15
-      tx_queue_data1_burst=3.0
-      tx_queue_data0_aifs=1
-      tx_queue_data0_cwmin=3
-      tx_queue_data0_cwmax=7
-      tx_queue_data0_burst=1.5
-
     '';
   };
 
