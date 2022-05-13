63d6a96eaef7fea9a1694629bef93c381eb843d1
Johan Andersson <johan-andersson01@protonmail.com>
2022-05-13 23:26:41 +0200

    WIP: patches for hostapd

--- sys/router.nix
+++ sys/router.nix
@@ -4,12 +4,221 @@
 
 { config, pkgs, lib, ... }:
 let patchedHostapd = pkgs.hostapd.overrideAttrs (old: rec {
-    patches = (old.patches or []) ++ [
-      (builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/openwrt/openwrt/eefed841b05c3cd4c65a78b50ce0934d879e6acf/package/network/services/hostapd/patches/300-noscan.patch";
-        sha256 = "08p5frxhpq1rp2nczkscapwwl8g9nc4fazhjpxic5bcbssc3sb00";
-      })
-   ];
+    patches = [
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/001-wolfssl-init-RNG-with-ECC-key.patch";
+            sha256 = "1h4wqn6dpc5vw19428v6s49i3xsdqc1ikwv6gvdhs2ly98cxwb91";
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/010-mesh-Allow-DFS-channels-to-be-selected-if-dfs-is-ena.patch";
+            sha256 = "06limshm4zprqd2cnjf2911k8dg2rc5wvdkqcdlxw49r5ihb4wmh";
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/011-mesh-use-deterministic-channel-on-channel-switch.patch";
+            sha256 = "1nkp8kmq1dxhrf19cz346jyaxh888vgvl9hnlsdqak5cb5g0k0a6";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/021-fix-sta-add-after-previous-connection.patch";
+            sha256 = "1nj4h8z8kz335cwz6qq1qd0k0h5c47nhqvpb6n4k0mabw3q19rd5";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/022-hostapd-fix-use-of-uninitialized-stack-variables.patch";
+            sha256 = "1sfy9j86550g90gw5w80773dgf6i1w22sidichxjqgkhdm507kz7";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/023-ndisc_snoop-call-dl_list_del-before-freeing-ipv6-add.patch";
+            sha256 = "03pq0h5lmlgn05dkd5vf0v3abaa30vza962vbp9kc66jbga38113";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/030-driver_nl80211-rewrite-neigh-code-to-not-depend-on-l.patch";
+            sha256 = "0vl3v7b2p17maxnz02jiy89rz6jbmj54sqxkw14j1s6mxji0x510";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/040-mesh-allow-processing-authentication-frames-in-block.patch";
+            sha256 = "0w56gr3lp6h2y88vc1g6ddjvrymrg9sv65bhq46dcjjc6i6fgq96";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/050-build_fix.patch";
+            sha256 = "19km2glb39nqd3a25dsh2mnv57yj8mywi0kln86rizncyv0wbp5f";
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/100-daemonize_fix.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/200-multicall.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/300-noscan.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/301-mesh-noscan.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/310-rescan_immediately.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/320-optional_rfkill.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/330-nl80211_fix_set_freq.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/340-reload_freq_change.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/341-mesh-ctrl-iface-channel-switch.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/350-nl80211_del_beacon_bss.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/360-ctrl_iface_reload.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/370-ap_sta_support.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/380-disable_ctrl_iface_mib.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/381-hostapd_cli_UNKNOWN-COMMAND.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/390-wpa_ie_cap_workaround.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/400-wps_single_auth_enc_type.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/410-limit_debug_messages.patch";
+            sha256 = lib.fakeSha256;
+            })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/420-indicate-features.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/430-hostapd_cli_ifdef.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/431-wpa_cli_ifdef.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/432-missing-typedef.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/450-scan_wait.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/460-wpa_supplicant-add-new-config-params-to-be-used-with.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/461-driver_nl80211-use-new-parameters-during-ibss-join.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/463-add-mcast_rate-to-11s.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/464-fix-mesh-obss-check.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/470-survey_data_fallback.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/500-lto-jobserver-support.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/590-rrm-wnm-statistics.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/599-wpa_supplicant-fix-warnings.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/600-ubus_support.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/610-hostapd_cli_ujail_permission.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/700-wifi-reload.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/710-vlan_no_bridge.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/711-wds_bridge_force.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/720-iface_max_num_sta.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/730-ft_iface.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/740-snoop_iface.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/750-qos_map_set_without_interworking.patch";
+            sha256 = lib.fakeSha256;
+        })
+        (builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/hostapd/patches/751-qos_map_ignore_when_unsupported.patch";
+            sha256 = lib.fakeSha256;
+        })
+    ];
+    src =  null;
+    srcs = [
+        (builtins.fetchGit {
+            url = "https://github.com/openwrt/openwrt.git";
+            ref = "master";
+            rev = "064e7e57b483e6879de0facef4f1fce86ec4ad47";
+        })
+        (builtins.fetchurl {
+            url = "https://w1.fi/releases/hostapd-2.10.tar.gz";
+            sha256 = "sha256-IG58eZtnhXLC49EgMCOHhLxKn4IyOwFWtMlGbxSYkV0=";
+        })
+    ];
+    sourceRoot = ".";
+    postUnpack = ''
+        cp -r source/package/network/services/hostapd/src/src ./hostapd-2.9
+        cd hostapd-2.10
+    '';
   });
 in
 {
@@ -165,7 +374,6 @@ qos_map_set=0,0,2,16,1,1,255,255,18,22,24,38,40,40,44,46,48,56
 bssid=06:f0:21:ac:39:fa
     '';
   };
-
   systemd.services.hostapd.path = [ patchedHostapd ];
 
   services.dnsmasq = {
