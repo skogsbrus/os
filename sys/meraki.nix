7475ff67111f59cb4dc0d4132bbd18d1437390fd
Johan Andersson <johan-andersson01@protonmail.com>
2022-05-21 08:47:44 +0200

    update udev rules

--- sys/meraki.nix
+++ sys/meraki.nix
@@ -3,6 +3,8 @@
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="4255", ATTR{idProduct}=="0012" MODE="0666"
     SUBSYSTEM=="usb", ATTR{idVendor}=="4255", ATTR{idProduct}=="0014" MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="4255", ATTR{idProduct}=="0001" MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0403", ATTR{idProduct}=="6010" MODE="0666"
   '';
 
   virtualisation.libvirtd.enable = true;
