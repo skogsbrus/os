bf45f0cfeb392cb60bf441f784e7558eacecf7d4
Johan Andersson <johan-andersson01@protonmail.com>
2022-05-30 17:22:59 +0200

    Add router config
    This commit introduces a new device, https://teklager.se/en/products/router-components/APU2E0-open-source-motherboard, to the fold.
    
    It has now fully replaced my old router, complete with ethernet, wifi, and a guest network.
    
    * Wifi is managed with hostapd, with patches from openwrt.
    * DNS and DHCP is managed by dnsmasq
    * Remaining network settings managed by builtin NixOS configs
    
    Future work:
    * Add uptime logging (grafana?)
    * Add anomaly detection (log / email)
    * Add pihole blacklists

--- sys/router.nix
+++ sys/router.nix
@@ -1,31 +1,111 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
-
 {
+  # https://github.com/mdlayher/homelab/blob/391cfc0de06434e4dee0abe2bec7a2f0637345ac/nixos/routnerr-2/configuration.nix#L38
+  # https://serverfault.com/questions/248841/ip-forwarding-when-and-why-is-this-required
+  boot = {
+    kernel = {
+      sysctl = {
+        # Forward on all interfaces.
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
+      };
+    };
+  };
+
+  imports = [
+    ./hostapd.nix
+  ];
+
   networking.hostName = "router";
   networking.useDHCP = false;
   networking.interfaces.enp1s0.useDHCP = true;
   networking.interfaces.enp2s0.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  networking.wireless.enable = true;
-  networking.wireless.userControlled.enable = true;
-  networking.networkmanager.unmanaged = [ "interface-name:wlp3s0" ] ++ lib.optional config.services.hostapd.enable "interface-name:${config.services.hostapd.interface}";
-  #services.hostapd = {
-  #  enable = true;
-  #  interface = "wlp3s0";
-  #  hwMode = "g";
-  #  ssid = "beepboop";
-  #  wpaPassphrase = "foobar";
-  #};
+  networking.nat = {
+    enable = true;
+    internalIPs = [
+      "192.168.1.0/24"
+      "192.168.2.0/24"
+    ];
+  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.bridges = {
+    br0 = {
+      interfaces = [
+        "enp2s0"
+        "wlp3s0"
+      ];
+    };
+  };
+
+  networking.interfaces = {
+    br0 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.1";
+          prefixLength = 24;
+        }
+      ];
+    };
+    wguest = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.2.1";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
+
+  networking.networkmanager.enable = false;
+
+  services.dnsmasq = {
+    enable = true;
+    servers = [ "9.9.9.9" "1.1.1.1" ];
+    extraConfig = ''
+      domain-needed
+      interface=br0
+      interface=wguest
+      dhcp-range=192.168.1.10,192.168.1.254,24h
+      dhcp-range=192.168.2.10,192.168.2.254,24h
+    '';
+  };
+
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = [ "br0" ];
+    allowedTCPPorts = [
+      # https://serverfault.com/a/424226
+      # DNS
+      53
+      80
+      # HTTP(S)
+      443
+      110
+      # Email (pop3, pop3s)
+      995
+      114
+      # Email (imap, imaps)
+      993
+      # Email (SMTP Submission RFC 6409)
+      587
+      # Git
+      2222
+    ];
+    allowedUDPPorts = [
+      # https://serverfault.com/a/424226
+      # DNS
+      53
+      # DHCP
+      67
+      68
+      # NTP
+      123
+    ];
+  };
+  # Prevent sshd from opening port 22 (circumventing the firewall)
+  services.openssh.openFirewall = false;
 }
-
