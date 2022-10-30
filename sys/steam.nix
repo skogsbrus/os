7b583d6c5115d17cfcba719a5dd19b6204b33dc0
Johan Andersson <johan-andersson01@protonmail.com>
2022-10-30 15:34:00 +0100

    Add steam link
    Taken from https://github.com/drewrisinger/nur-packages/pull/78

--- sys/steam.nix
+++ sys/steam.nix
@@ -1,5 +1,4 @@
-{
-  config
+{ config
 , lib
 , pkgs
 , ...
@@ -7,13 +6,15 @@
 let
   cfg = config.skogsbrus.steam;
   inherit (lib) genAttrs mkOption types mkIf mkEnableOption;
+  steamlink = (pkgs.callPackage ./steamlink { });
 in
 {
 
   options.skogsbrus.steam = {
     enable = mkEnableOption "steam";
+    steamlink = mkEnableOption "link";
     users = mkOption {
-      default = [];
+      default = [ ];
       description = "Users to add to the 'input' group";
       example = [ "user1" "user2" ];
       type = types.listOf types.str;
@@ -25,5 +26,12 @@ in
 
     programs.steam.enable = true;
     hardware.steam-hardware.enable = true;
+
+    config.environment.systemPackages = (
+      if cfg.steamlink then
+        [ steamlink ]
+      else
+        [ ]
+    );
   };
 }
