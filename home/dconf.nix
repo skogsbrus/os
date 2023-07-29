be36dec39abc64e0b6fa90dae629e2fef86986b5
Johan Andersson <contact@skogsbrus.xyz>
2023-07-29 13:16:39 +0200

    Replace xfce4 with kgx

--- home/dconf.nix
+++ home/dconf.nix
@@ -589,7 +589,7 @@ with lib.hm.gvariant;
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>Return";
-      command = "xfce4-terminal";
+      command = "kgx";
       name = "Launch terminal";
     };
 
