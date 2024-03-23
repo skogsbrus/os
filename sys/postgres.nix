9b9271fbaecfbc683c26051f2957b03f7d62d80a
Johan Andersson <contact@skogsbrus.xyz>
2024-03-23 08:49:17 +0100

    postgres: pin to version 14

--- sys/postgres.nix
+++ sys/postgres.nix
@@ -22,7 +22,7 @@ in
   config = mkIf cfg.enable {
     services.postgresql = {
       enable = true;
-      package = pkgs.postgresql;
+      package = pkgs.postgresql_14;
       enableTCPIP = false;
       authentication = pkgs.lib.mkForce ''
         # Generated file; do not edit!
