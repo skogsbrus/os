51ebb52dd4df46ecc9d466444cdd8b25f555ae6e
Johan Andersson <contact@skogsbrus.xyz>
2024-04-09 22:36:12 +0200

    Keeper: rekey secrets
    Rekey secrets to allow new host key of Keeper.

--- secrets.nix
+++ secrets.nix
@@ -2,9 +2,10 @@ let
   johanan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINug6YZP5It5utF3UALqq+Wq93Taj+xtzaOMv6qwVfWc contact@skogsbrus.xyz";
   keeper = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINrqJCVHXS7bxmyOtRlhR6YzgY6bFqTyrzfctHA+1NBg root@keeper";
   router = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZ7KL8gkdoBcFYF1AJUQgmXn0xUbl02nFYScqCV6kwT root@router";
+  keeper2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGM23/K1hDtf6weSQbfuhLnMpg54KIumxlu+hQXxyKe0 root@keeper";
 
   user_keys = [ johanan ];
-  host_keys = [ keeper router ];
+  host_keys = [ keeper2 router ];
 in
 {
   "secrets/authelia_cfg_yaml.age".publicKeys = user_keys ++ host_keys;
