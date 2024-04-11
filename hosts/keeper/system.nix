976516774829d7611d0b1316014c9553b975690a
Johan Andersson <contact@skogsbrus.xyz>
2024-04-11 22:39:20 +0200

    re-enable rclone backup for /tank

--- hosts/keeper/system.nix
+++ hosts/keeper/system.nix
@@ -110,16 +110,16 @@ in
     };
 
     rcloneBackup = {
-      enable = false;
+      enable = true;
       b2_directories = {
         "/tank/media/photos" = "keeper/tank/media/photos";
         "/tank/media/books" = "keeper/tank/media/books";
-        "/var/lib/mysql" = "keeper/var/lib/mysql";
-        "/var/lib/postgresql" = "keeper/var/lib/postgresql";
-        "/var/lib/paperless" = "keeper/var/lib/paperless";
-        "/var/lib/photoprism" = "keeper/var/lib/photoprism";
-        "/var/lib/grafana" = "keeper/var/lib/grafana";
-        "/var/lib/prometheus2" = "keeper/var/lib/prometheus2";
+        #"/var/lib/mysql" = "keeper/var/lib/mysql";
+        #"/var/lib/postgresql" = "keeper/var/lib/postgresql";
+        #"/var/lib/paperless" = "keeper/var/lib/paperless";
+        #"/var/lib/photoprism" = "keeper/var/lib/photoprism";
+        #"/var/lib/grafana" = "keeper/var/lib/grafana";
+        #"/var/lib/prometheus2" = "keeper/var/lib/prometheus2";
       };
     };
 
